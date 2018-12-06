f8cdf4bd94fcf0868f68b916e8baf165e26d6c6d
wahib <wahib.med.ouahib@gmail.com>
2018-12-06 00:01:25 +0000

    manupilating the dom with state

--- src/app/index.js
+++ src/app/index.js
@@ -4,13 +4,27 @@ var ReactDOM = require('react-dom')
 
 //Create component
 var TodoComponent = React.createClass({
+    getInitialState: function(){
+        return{
+            todos: ['sport','sleep','code'],
+            age: 22
+        }
+    },
     render: function(){
+        var ager = setTimeout(function(){
+            this.setState({
+                age: 23
+            })
+        }.bind(this),5000);
         return(
-            <div>
-                <h1>heeeeeeeeey my name is</h1>
-                <p>{this.props.msg}</p>
-                <p><strong>name: </strong>{this.props.pet.name}</p>
-                <p><strong>age: </strong>{this.props.pet.age}</p>
+            <div id="todo-list">
+                <p>my age: {this.state.age}</p>
+                <p>my todos</p>
+                <ul>
+                    <li>{this.state.todos[0]}</li>
+                    <li>{this.state.todos[1]}</li>
+                    <li>{this.state.todos[2]}</li>
+                </ul>
             </div>
         );
     }
