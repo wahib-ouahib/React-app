d16a5423719cf90ea8c9f0d8e8772e066bba4ef1
wahib <wahib.med.ouahib@gmail.com>
2018-12-06 23:00:22 +0000

    :white_check_mark: Modularizing the code

---
+++ src/app/todoitem.js
@@ -0,0 +1,21 @@
+
+//Create TodoItem Component
+var TodoItem = React.createClass({
+    render: function(){
+        return(
+            <li>
+                <div className="todo-item">
+                    <span className="item-name">{this.props.item}</span>
+                    <span className="item-delete" onClick={this.handleDelete}> x </span>
+                </div>
+            </li>
+        );
+    },
+
+    //Custom functions
+    handleDelete: function(){
+        this.props.onDelete(this.props.item);
+    }
+})
+
+module.exports = TodoItem;
